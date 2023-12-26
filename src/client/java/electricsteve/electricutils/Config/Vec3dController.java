ecf3826357a2841a748ad78f9a6c0c3e7828c62b
ElectricSteve <nathanndees@gmail.com>
2023-12-26 15:28:50 +0100

    Added some stuff that isn't really used yet.

--- src/client/java/electricsteve/electricutils/Config/Vec3dController.java
+++ src/client/java/electricsteve/electricutils/Config/Vec3dController.java
@@ -8,9 +8,11 @@ import dev.isxander.yacl3.gui.YACLScreen;
 import dev.isxander.yacl3.gui.controllers.ActionController;
 import dev.isxander.yacl3.gui.controllers.ControllerWidget;
 import dev.isxander.yacl3.gui.utils.GuiUtils;
+import dev.isxander.yacl3.gui.utils.UndoRedoHelper;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.math.Vec3d;
 
 public class Vec3dController implements Controller<Vec3d> {
@@ -24,6 +26,17 @@ public class Vec3dController implements Controller<Vec3d> {
     public Vec3dController(Option<Vec3d> option, Text text) {
         this.option = option;
         this.text = text;
+    }
+
+    // Getters
+    public double getX() {
+        return option().pendingValue().x;
+    }
+    public double getY() {
+        return option().pendingValue().y;
+    }
+    public double getZ() {
+        return option().pendingValue().z;
     }
 
     @Override
@@ -42,32 +55,69 @@ public class Vec3dController implements Controller<Vec3d> {
     }
 
     public static class Vec3dControllerWidget extends ControllerWidget<Vec3dController> {
+
+        // Input fields
+        protected Double inputFieldX;
+        protected Dimension<Integer> inputFieldXBounds;
+        protected boolean inputFieldXFocused;
+        
+        protected Double inputFieldY;
+        protected Dimension<Integer> inputFieldYBounds;
+        protected boolean inputFieldYFocused;
+        
+        protected Double inputFieldZ;
+        protected Dimension<Integer> inputFieldZBounds;
+        protected boolean inputFieldZFocused;
+        
+        
+        protected int caretPos;
+        protected int selectionLength;
+        protected int renderOffset;
+
+        protected float ticks;
+
+        private final Text emptyText;
+
         protected boolean hovered1;
         protected boolean hovered2;
         protected boolean hovered3;
 
         public Vec3dControllerWidget(Vec3dController control, YACLScreen screen, Dimension<Integer> dim) {
             super(control, screen, dim);
+            inputFieldX = control.getX();
+            inputFieldY = control.getY();
+            inputFieldZ = control.getZ();
+
+            inputFieldXFocused = false;
+            inputFieldYFocused = false;
+            inputFieldZFocused = false;
+
+            selectionLength = 0;
+            emptyText = Text.literal("Click to type...").formatted(Formatting.GRAY);
+            control.option().addListener((opt, val) -> {
+                inputFieldX = control.getX();
+            });
+            setDimension(dim);
         }
 
         @Override
         public void render(DrawContext graphics, int mouseX, int mouseY, float delta) {
             Dimension<Integer> dim = getDimension();
-            Dimension<Integer> dim1 = Dimension.ofInt(dim.x(), dim.y(), dim.width() / 3, dim.height());
-            Dimension<Integer> dim2 = Dimension.ofInt(dim.x() + dim.width() / 3, dim.y(), dim.width() / 3, dim.height());
-            Dimension<Integer> dim3 = Dimension.ofInt(dim.x() + dim.width() / 3 * 2, dim.y(), dim.width() / 3, dim.height());
+            inputFieldXBounds = Dimension.ofInt(dim.x(), dim.y(), dim.width() / 3, dim.height());
+            inputFieldYBounds = Dimension.ofInt(dim.x() + dim.width() / 3, dim.y(), dim.width() / 3, dim.height());
+            inputFieldZBounds = Dimension.ofInt(dim.x() + dim.width() / 3 * 2, dim.y(), dim.width() / 3, dim.height());
 
             hovered = isMouseOver(mouseX, mouseY);
-            hovered1 = isMouseOver(mouseX, mouseY, dim1);
-            hovered2 = isMouseOver(mouseX, mouseY, dim2);
-            hovered3 = isMouseOver(mouseX, mouseY, dim3);
+            hovered1 = isMouseOver(mouseX, mouseY, inputFieldXBounds);
+            hovered2 = isMouseOver(mouseX, mouseY, inputFieldYBounds);
+            hovered3 = isMouseOver(mouseX, mouseY, inputFieldZBounds);
 
             Text name = control.option().changed() ? modifiedOptionName : control.option().name();
             Text shortenedName = Text.literal(GuiUtils.shortenString(name.getString(), textRenderer, getDimension().width() - getControlWidth() - getXPadding() - 7, "...")).setStyle(name.getStyle());
 
-            drawButtonRect(graphics, dim1.x(), dim1.y(), dim1.xLimit(), dim1.yLimit(), hovered1 || focused, isAvailable());
-            drawButtonRect(graphics, dim2.x(), dim2.y(), dim2.xLimit(), dim2.yLimit(), hovered2 || focused, isAvailable());
-            drawButtonRect(graphics, dim3.x(), dim3.y(), dim3.xLimit(), dim3.yLimit(), hovered3 || focused, isAvailable());
+            drawButtonRect(graphics, inputFieldXBounds.x(), inputFieldXBounds.y(), inputFieldXBounds.xLimit(), inputFieldXBounds.yLimit(), hovered1 || focused, isAvailable());
+            drawButtonRect(graphics, inputFieldYBounds.x(), inputFieldYBounds.y(), inputFieldYBounds.xLimit(), inputFieldYBounds.yLimit(), hovered2 || focused, isAvailable());
+            drawButtonRect(graphics, inputFieldZBounds.x(), inputFieldZBounds.y(), inputFieldZBounds.xLimit(), inputFieldZBounds.yLimit(), hovered3 || focused, isAvailable());
 
             graphics.drawText(textRenderer, shortenedName, getDimension().x() + getXPadding(), getTextY(), getValueColor(), true);
 
@@ -88,19 +138,13 @@ public class Vec3dController implements Controller<Vec3d> {
             if (!isMouseOver(mouseX, mouseY) || !isAvailable())
                 return false;
 
-            OptionClicked();
+
             return true;
         }
 
         @Override
         protected int getHoveredControlWidth() {
             return getUnhoveredControlWidth();
-        }
-
-        public void OptionClicked() {
-
-
-            playDownSound();
         }
 
         @Override
@@ -115,7 +159,7 @@ public class Vec3dController implements Controller<Vec3d> {
             }
 
             if (keyCode == InputUtil.GLFW_KEY_ENTER || keyCode == InputUtil.GLFW_KEY_SPACE || keyCode == InputUtil.GLFW_KEY_KP_ENTER) {
-                OptionClicked();
+
                 return true;
             }
 
